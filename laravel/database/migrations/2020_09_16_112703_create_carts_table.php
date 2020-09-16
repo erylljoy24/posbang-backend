25d3552e04cab186461ad7d0bd482f6c042be8fe
erylljoy24 <eryll.salamanes24@gmail.com>
2020-09-16 20:50:18 +0800

    Update

---
+++ laravel/database/migrations/2020_09_16_112703_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('cat_id')->unsigned();
+            $table->foreign('cat_id')->references('id')->on('categories');
+            $table->unsignedBigInteger('menu_id')->unsigned();
+            $table->foreign('menu_id')->references('id')->on('menus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
