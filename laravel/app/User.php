55d9e5cb51c69718aa3c6ae3ebd4ba4b5af749b5
erylljoy24 <eryll.salamanes24@gmail.com>
2020-09-17 10:15:38 +0800

    Update

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +20,7 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
-    /**
+    /**AuthServiceProvider
      * The attributes that should be hidden for arrays.
      *
      * @var array
